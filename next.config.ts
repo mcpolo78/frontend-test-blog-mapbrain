d40ee50c8d396dd5fe88b19447b389c6e3297944
prenom nom <mcpolo78@users.noreply.github.com>
2025-09-24 20:14:25 +0200

    eslint

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // Ignorer les erreurs ESLint pendant le build
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
